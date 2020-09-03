dd803b6620cb2fd5102e21aaf8beec27a00bcb10
Daniils <daniils.korolenko@if.lv>
2020-09-03 15:15:43 +0300

    Improved feature

--- api_tests/test/Steps/postman-echo-service-steps.js
+++ api_tests/test/Steps/postman-echo-service-steps.js
@@ -1,39 +1,40 @@
 const { Given, When, Then } = require('cucumber');
 const fetch = require('node-fetch');
 let url;
-let cookies;
-let cookieToBeRemoved;
+let paramsToBePosted;
+let paramsToBeRemoved;
+let fullUrl;
 
 Given('postman echo service', function () {
     url = 'https://postman-echo.com/';
 });
 
 When('I set three cookies', function () {
-    cookies = "cookie1=chocko&cookie2=white&cookie3=black";
+    paramsToBePosted = "cookie1=chocko&cookie2=white&cookie3=black";
 });
 
 When('I set url to retrieve all cookies', function () {
-    url + 'cookies'
+    fullUrl = url + 'cookies';
 });
 
 When('I create a parameter to delete a specific cookie', function () {
-    cookieToBeRemoved = "cookie1=chocko";
+    paramsToBeRemoved = "cookie1=chocko";
 });
 
 Then('the cookies are set', function () {
-    fetch(url + 'cookies/set?' + cookies)
+    fetch(fullUrl+ '/set?' + paramsToBePosted)
     .then(res => res.json())
     .then(json => console.log(json));
 });
 
 Then('the set of cookies is retrieved', function () {
-    fetch(url + 'cookies')
+    fetch(fullUrl)
     .then(res => res.json())
     .then(json => console.log(json));
 });
 
 Then('the cookie is removed from the set', function () {
-    fetch(url + 'cookies/delete?' + cookieToBeRemoved)
+    fetch(fullUrl + '/delete?' + paramsToBeRemoved)
     .then(res => res.json())
     .then(json => console.log(json));
 });
